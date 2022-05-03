c52c12a7c28abf469122e99e62e07122a12435ad
Larry <raijin22@outlook.com>
2022-05-02 21:34:30 -0800

    practice

---
+++ practice_c.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+/*
+//square
+int main(){
+  printf("  /|\n");
+  printf(" / |\n");
+  printf("/__|\n");
+  return 0;
+}
+*/
+
+/*
+// Sentence
+int main(){
+  char name[] = "Olivia";
+  int age = 25;
+  char drink[] = "wine";
+
+  printf("Her name is %s and she is %d.\n", name, age);
+  printf("Olivia likes to drink %s.\n", drink);
+  printf("%s likes to drink %s that is aged %d years.", name, drink, age);
+  return 0 ;
+}
+*/
+
+int main(){
+  double x = 3.42;
+  printf("%f", pow(2,3));
+  return 0 ;
+}
